903655e419f424db104f4301d690a6baa2cdcaa5
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-01-08 11:15:56 +0000

    Item697: Argh, we have to be very selective: some SYSTEMWEBs are correct already, some are not.
    git-svn-id: http://svn.foswiki.org/trunk@1874 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- JQueryTwistyPlugin/lib/TWiki/Plugins/JQueryTwistyPlugin.pm
+++ JQueryTwistyPlugin/lib/TWiki/Plugins/JQueryTwistyPlugin.pm
@@ -104,7 +104,7 @@ sub _addHeader {
 @import url("%PUBURL%/%TWIKIWEB%/JQueryTwistyPlugin/twist.css");
 </style>
 <script type="text/javascript" src="%PUBURL%/%TWIKIWEB%/JQueryTwistyPlugin/twist.js"></script>
-<script type="text/javascript" src="%PUBURL%/%TWIKIWEB%/JavascriptFiles/foswikiPref.js"></script>
+<script type="text/javascript" src="%PUBURL%/%SYSTEMWEB%/JavascriptFiles/foswikiPref.js"></script>
 <script type="text/javascript">
 // <![CDATA[
 var styleText = '<style type="text/css" media="all">.twikiMakeVisible{display:inline;}.twikiMakeVisibleInline{display:inline;}.twikiMakeVisibleBlock{display:block;}.twikiMakeHidden{display:none;}</style>';
