ccf25b15381abaf496ef5f4d7824409ae9cf1d9c
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-04-30 00:18:50 +0000

    Item1175: Adding PersonalInfoAddOn to foswiki.org
    git-svn-id: http://svn.foswiki.org/branches/foswiki.org@3766 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ PersonalInfoAddOn/lib/Foswiki/Contrib/PersonalInfoAddOn/build.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+#
+# Build for PersonalInfoAddOn
+# version 1.0.1
+
+BEGIN {
+  foreach my $pc (split(/:/, $ENV{FOSWIKI_LIBS})) {
+    unshift @INC, $pc;
+  }
+}
+
+use Foswiki::Contrib::Build;
+
+# Create the build object
+$build = new Foswiki::Contrib::Build( 'PersonalInfoAddOn' );
+
+# Build the target on the command line, or the default target
+$build->build($build->{target});
+
