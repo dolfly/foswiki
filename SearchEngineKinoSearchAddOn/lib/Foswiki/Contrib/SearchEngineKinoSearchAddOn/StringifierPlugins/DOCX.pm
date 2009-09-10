335b4c0d311542beee3380fd34cf97fc38374319
AndrewJones <AndrewJones@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-09-10 16:00:09 +0000

    Item1363: Strinigifiers for .docx, .xlsx and .pptx
    git-svn-id: http://svn.foswiki.org/trunk@4828 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ SearchEngineKinoSearchAddOn/lib/Foswiki/Contrib/SearchEngineKinoSearchAddOn/StringifierPlugins/DOCX.pm
@@ -0,0 +1,58 @@
+# Copyright (C) 2009 TWIKI.NET (http://www.twiki.net)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# For licensing info read LICENSE file in the Foswiki root.
+
+package Foswiki::Contrib::SearchEngineKinoSearchAddOn::StringifyPlugins::DOCX;
+use base 'Foswiki::Contrib::SearchEngineKinoSearchAddOn::StringifyBase';
+use Foswiki::Contrib::SearchEngineKinoSearchAddOn::Stringifier;
+use File::Temp qw/tmpnam/;
+use Encode;
+use CharsetDetector;
+
+# Only if ppthtml exists, I register myself.
+if (__PACKAGE__->_programExists("docx2txt.pl")){
+    __PACKAGE__->register_handler("text/docx", ".docx");
+}
+
+sub stringForFile {
+    my ($self, $filename) = @_;
+    my $tmp_file = tmpnam();
+
+    my $cmd = "docx2txt.pl '$filename' $tmp_file 2>/dev/null";
+    return "" unless ((system($cmd) == 0) && (-f $tmp_file));
+  
+    my $in;
+    open $in, $tmp_file or return "";
+    my $text = "";
+   
+     while (<$in>) {
+        chomp;
+
+        my $charset = CharsetDetector::detect1($_);
+        my $aux_text = "";
+        if ($charset =~ "utf") {
+            $aux_text = encode("iso-8859-15", decode($charset, $_));
+            $aux_text = $_ unless($aux_text);
+        } else {
+            $aux_text = $_;
+        }
+        $text .= " " . $aux_text;
+    }
+
+    close($in);
+    unlink($tmp_file);
+    
+    return $text;
+}
+
+1;
